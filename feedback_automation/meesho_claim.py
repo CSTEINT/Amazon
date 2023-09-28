d90e44d4d6269d8cd5d8fd20ab36a77e4f1c195b
venu-cste-international <venu@cste.international>
2023-09-28 23:06:11 +0530

    updated changes

--- feedback_automation/meesho_claim.py
+++ feedback_automation/meesho_claim.py
@@ -29,13 +29,9 @@ def run(playwright: Playwright) -> None:
     page.get_by_label("Password").fill(login['password'])
     page.get_by_role("button", name="Log in").click()
     page.wait_for_load_state('networkidle')
-    # page.pause()
     ids= ['#product_image_link_6','#product_reverse_way_bill_link_7','#product_openingvideo_link_8']
     ls = ["product","reverse_bill",'openingvideo']
-    time.sleep(5
-
-
-            )
+    time.sleep(5)
     for idx in range(len(order_ids)):
         if order_ids[idx] not in claimed_lst:
             page.goto("https://supplier.meesho.com/panel/v3/new/payouts/wbixy/support/1/3/create")
@@ -43,22 +39,20 @@ def run(playwright: Playwright) -> None:
                 for file in os.listdir('meesho_claims'):
                     if f'{ls[each]}_{order_ids[idx]}' in file:
                         page.locator(ids[each]).set_input_files('\\'.join([str(os.getcwd()),'meesho_claims',file]))
+            page.wait_for_selector('#mui-6')
             page.click('//*[@id="mainWrapper"]/div/div/div/form/div[1]/div[3]/div/div')
             page.click('text=Intact')
-            page.locator("//textarea[@id='mui-6']").fill(df['Comments'][idx])
-            time.sleep(30)
-            # page.pause()
-            page.click("//input[@id='mui-4']")
-            page.locator("//input[@id='mui-4']").fill(df['Packet_ID'][idx])
-            time.sleep(10)
-            page.click("//input[@id='mui-3']")
-            page.locator("//input[@id='mui-3']").fill(order_ids[idx])
-            time.sleep(5)
-            # page.click('//*[@id="mainWrapper"]/div/div/div/form/div[3]/div/button[1]/span')
+            page.locator("//textarea[@placeholder='']").fill(df['Comments'][idx])
+            time.sleep(3)
+            page.get_by_placeholder("Packet ID").click()
+            page.get_by_placeholder("Packet ID").fill(str(df['Packet_ID'][idx]))
+            time.sleep(3)
+            page.get_by_placeholder("Sub Order Number").click()
+            page.get_by_placeholder("Sub Order Number").fill(str(order_ids[idx]))
+            page.click('//*[@id="mainWrapper"]/div/div/div/form/div[3]/div/button[1]/span')
             time.sleep(10)
             claimed_lst.append(order_ids[idx])
             logger.info(f"Successffuly Claimed for the order_id : {order_ids[idx]}")
-            # https://supplier.meesho.com/panel/v3/new/payouts/wbixy/support/1/3/create
     # ---------------------
     page.close()
     context.close()
@@ -67,12 +61,15 @@ def run(playwright: Playwright) -> None:
 
 if __name__=="__main__":
     logger.info(f"script started at {datetime.now()}")
-    df = pd.read_csv('meesho_claims.csv')
-    order_ids = [x for x in df['Sub_Order_Id']]
+    if not os.path.exists("meesho_claims"):
+        os.makedirs("meesho_claims")
+    dowloaded = os.listdir('meesho_claims')
+    opt = [login['drive_link']]
+    main(opt, dowloaded)
+    df = pd.read_csv('meesho_claims//return_claims_meesho.csv')
+    order_ids = [x for x in df['Order_Id']]
     df1 = pd.read_csv('claimed_list_meesho.csv')
     claimed_lst = [x for x in df1['Order_Id']]
-    opt = parse_opt()
-    main(opt)
     with sync_playwright() as playwright:
         run(playwright)
     df1 = pd.DataFrame({'Order_Id': claimed_lst})
