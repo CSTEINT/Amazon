28e4e438d980459c83759c8a25408b5d086fad99
venu-cste-international <venu@cste.international>
2023-10-08 13:08:06 +0530

    add clicker Script

--- feedback_automation/ad_clicker.py
+++ feedback_automation/ad_clicker.py
@@ -8,6 +8,7 @@ import schedule
 from pyotp import *
 import logging
 from nanoid import generate
+from emails import get_sheet_and_timings, send_mail
 import re
 import logging.config
 import json
@@ -38,8 +39,11 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def loop(page):
-	tree = html.fromstring(page.content())
+def loop(data):
+	temp_tar_spon = []
+	temp_our_pro = []
+	temp_tar_pro = []
+	tree = html.fromstring(data)
 	spon_lst = []
 	prod_lst = []
 	all_prod_pages = tree.xpath('//a[@class="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"]/@href')
@@ -48,16 +52,21 @@ def loop(page):
 	for i  in range(len(spon_lst)):
 		final_pair.append({'owner':spon_lst[i],'url':''.join(['https://www.amazon.in',all_prod_pages[i]])})
 	for each in final_pair:
-		if ('spc=' in each['url']) and ('Fusked' not in each['owner']) and ('Caaju' not in each['owner']):
-			tar_spon.append(each)
+		if ('spc=' in each['url']) and ('fusked' not in each['owner'].lower()) and ('caaju' not in each['owner'].lower()):
+			temp_tar_spon.append(each)
 		else: 
-			if ('Fusked' in each['owner']) or ('Caaju' in each['owner']):
-				our_pro.append(each)
+			if ('fusked' in each['owner'].lower()) or ('caaju' in each['owner'].lower()):
+				temp_our_pro.append(each)
 			else:
-				tar_pro.append(each)
+				temp_tar_pro.append(each)
+	return temp_tar_spon,temp_tar_pro,temp_our_pro
+
 
 
 def run(playwright: Playwright,phrase):
+	tar_spon = []
+	tar_pro = []
+	our_pro = []
 	browser = playwright.chromium.launch(channel="chrome", headless=False, proxy= {
 		'server': login['p_server'],
 		'username': login['p_username'],
@@ -67,16 +76,13 @@ def run(playwright: Playwright,phrase):
 	stealth_sync(context)
 	page = context.new_page()
 	try:
-		page.goto(login['proxy_url'])
-	except TimeoutError:
-		pass
-	try:
-		page.goto('https://www.amazon.in')
+		page.goto(login['proxy_url'],timeout=10000)
 	except TimeoutError:
 		page.close()
 		browser.close()
 		return run(playwright,phrase)
 	time.sleep(5)
+	page.goto('https://www.amazon.in')
 	if 'Enter the characters you see below' in page.content(): 
 		time.sleep(20)
 		page.close()
@@ -86,37 +92,39 @@ def run(playwright: Playwright,phrase):
 	page.get_by_placeholder("Search Amazon.in").press("Enter")
 	time.sleep(10)
 	tree = html.fromstring(page.content())
-	pages = tree.xpath("//div[@class='a-section a-text-center s-pagination-container']//a//text()")
-	l_pages =[x for x in pages if x not in ['Next','Previous']]
-	loop(page)
-	for idx in range(len(l_pages)):
+	for idx in range(5):
 		page.get_by_text("Next", exact=True).click()
-		time.sleep(10)
-		loop(page)
+		page.wait_for_selector('.s-pagination-strip')
+		time.sleep(3)
+		data = page.content()
+		s1,p1,o1 = loop(data)
+		tar_spon += s1
+		tar_pro += p1 
+		our_pro += o1
 	for each in tar_spon:
 		page.goto(each['url'],timeout=60000)
 		logger.info(f"Clicked to Ad Sponsored by {each['owner']} at timestamp {datetime.now()}")
 	for each in our_pro:
+		page.goto(each['url'],timeout=60000)
 		logger.info(f"Clicked to our products at timestamp {datetime.now()}")
-		page.goto(each['url'],timeout=60000)
 	for each in tar_pro[:3]:
-		logger.info(f"Clicked to competetors products at timestamp {datetime.now()}")
 		page.goto(each['url'],timeout=60000)
+		logger.info(f"Clicked to competetors products at timestamp {datetime.now()}")
 	page.close()
 	browser.close()
 	return True
 
 
 if __name__=="__main__":
-	get_sheet_and_timings()
-	df = pd.read_csv('ad_clicker.csv')
-	logger.info(f"Automation Started at {datetime.now()}")
-	lst = [x for x in df['phrases']]
-	tar_spon = []
-	tar_pro = []
-	our_pro = []
-	for phrase in lst:
-		with sync_playwright() as playwright:
-			run(playwright,phrase)
-	send_mail(log_file)
-	logger.info(f"Automation ended at {datetime.now()}")
+	while(True):
+		get_sheet_and_timings()
+		logger.info(f"Automation Started at {datetime.now()}")
+		df = pd.read_csv('ad_clicker.csv')
+		lst = [x for x in df['phrases']]
+		for phrase in lst:
+			with sync_playwright() as playwright:
+				run(playwright,phrase)
+		send_mail(log_file)
+		logger.info(f"Automation ended at {datetime.now()}")
+		logger.info(f"Automation Will Restart after 5 minutes.")
+		time.sleep(300)
